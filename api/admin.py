22e0f57bec3452c2816915df1442a5adb0261e7b
Pranima Kansakar <kpranima23@gmail.com>
2021-06-21 14:36:22 +0545

    models for user, items, tables, orders and reservations

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,86 @@
 from django.contrib import admin
+from django.contrib.auth import get_user_model
+from django.contrib.auth.admin import UserAdmin
+from django.utils.translation import ugettext_lazy as _
+# from import_export.admin import ImportExportModelAdmin
+from .models import *
 
-# Register your models here.
+#User
+class CustomUserAdmin(UserAdmin):    
+    
+    # model = User
+    # fieldsets = UserAdmin.fieldsets + (
+    #     (None, {'fields': ('phone_no',)}),
+    # )
+    # add_fieldsets = UserAdmin.add_fieldsets + (
+    #     (None, {'fields': ('phone_no',)}),
+    # )
+    # list_display = UserAdmin.list_display + ('phone_no',)
+    # search_fields = ('email', 'phone_no')
+
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        (_('Personal info'), {'fields': ('first_name', 'last_name', 'phone_no')}),
+        (_('Permissions'), {'fields': ('is_active', 'is_staff', 'is_superuser',
+                                       'groups', 'user_permissions')}),
+        (_('Important dates'), {'fields': ('last_login', 'date_joined')}),
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'phone_no', 'password1', 'password2'),
+        }),
+    )
+    list_display = ('email', 'first_name', 'last_name', 'phone_no', 'is_staff')
+    search_fields = ('email', 'first_name', 'last_name', 'phone_no')
+    ordering = ('email',)
+
+#Item
+class ItemAdmin(admin.ModelAdmin):
+    list_display = ('name', 'cost', 'categories', 'ingredients', 'offer', 'discount', 'cost_after_discount')
+
+#Category
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name', 'parent_category')
+
+#Ingredient
+class IngredientAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+
+class IngredientsInItemAdmin(admin.ModelAdmin):
+    list_display = ('item', 'ingredient', 'quantity_of_ingredient',)
+
+#Offer
+class OfferAdmin(admin.ModelAdmin):
+    list_display = ('name', 'discount_rate')
+
+#Table and Seat Plan
+class TableAdmin(admin.ModelAdmin):
+    list_display = ('table_no', 'party_size_capacity')
+
+class SeatPlanAdmin(admin.ModelAdmin):
+    list_display = ('id', 'party_size_capacity', 'tables')
+
+#Reservation
+class ReservationAdmin(admin.ModelAdmin):
+    list_display = ('id', 'date_reserved', 'check_in_date', 'party_size', 'seat_plan')
+
+#Order
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ('id', 'date_ordered', 'reservation_detail', 'offer', 'order_details', 'order_total_cost', 'total_cost_after_discount')
+
+#Order Detail
+class OrderDetailAdmin(admin.ModelAdmin):
+    list_display = ('id', 'order', 'item', 'quantity', 'order_item_cost', 'order_item_cost_after_discount', 'special_instruction')
+
+admin.site.register(User, CustomUserAdmin)
+admin.site.register(Item, ItemAdmin)
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Ingredient, IngredientAdmin)
+admin.site.register(IngredientsInItem, IngredientsInItemAdmin)
+admin.site.register(Offer, OfferAdmin)
+admin.site.register(Table, TableAdmin)
+admin.site.register(SeatPlan, SeatPlanAdmin)
+admin.site.register(Reservation, ReservationAdmin)
+admin.site.register(Order, OrderAdmin)
+admin.site.register(OrderDetail, OrderDetailAdmin)
--- api/models.py
+++ api/models.py
@@ -1,3 +1,205 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser, BaseUserManager
+from django.utils.translation import ugettext_lazy as _
 
-# Create your models here.
+#User model
+class CustomUserManager(BaseUserManager):
+    def _create_user(self, email, password, **extra_fields):
+        """Create and save a User with the given email and password."""
+        if not email:
+            raise ValueError('The given email must be set')
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_user(self, email, password=None, **extra_fields):
+        """Create and save a regular User with the given email and password."""
+        extra_fields.setdefault('is_staff', False)
+        extra_fields.setdefault('is_superuser', False)
+        return self._create_user(email, password, **extra_fields)
+
+    def create_superuser(self, email, password, **extra_fields):
+        """Create and save a SuperUser with the given email and password."""
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError('Superuser must have is_staff=True.')
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError('Superuser must have is_superuser=True.')
+
+        return self._create_user(email, password, **extra_fields)
+
+
+class User(AbstractUser):
+    username = None
+    email = models.EmailField(_('email address'), unique=True)
+    phone_no = models.CharField(max_length=10, unique=True)
+
+    objects = CustomUserManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+    def __str__(self):
+        return self.email
+
+#Offer model
+class Offer(models.Model):
+    name = models.CharField(max_length=100, null=True, blank=True)
+    discount_rate = models.DecimalField(help_text="in percent", max_digits=4, decimal_places=2, null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+#Category model
+class Category(models.Model):
+    class Meta:
+        verbose_name_plural = 'Categories'
+
+    name = models.CharField(max_length=50, null=True, blank=True)
+    parent_category = models.ForeignKey('self', on_delete=models.SET_NULL, null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+
+
+#Ingredient model
+class Ingredient(models.Model):
+    name = models.CharField(max_length=50, null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+
+#Item model
+class Item(models.Model):
+    name = models.CharField(max_length=50, null=True, blank=True)
+    cost = models.DecimalField(max_digits=6, decimal_places=2, null=True, blank=True)
+    category = models.ManyToManyField(Category)
+    # image = models.ImageField(null=True)
+    description = models.TextField(null=True, blank=True)
+    ingredient = models.ManyToManyField(Ingredient, through='IngredientsInItem')
+    offer = models.ForeignKey(Offer, on_delete=models.SET_NULL, null=True, blank=True)
+    
+    @property
+    def discount(self):
+        if self.offer:
+            return self.cost * self.offer.discount_rate / 100
+        else:
+            return 0
+
+    @property
+    def cost_after_discount(self):
+        return self.cost - self.discount
+
+    def __str__(self):
+        return self.name
+
+    def categories(self):
+        if self.category:
+            return ', '.join([c.name for c in self.category.all()])
+
+    def ingredients(self):
+        if self.ingredient:
+            return ', '.join([i.name for i in self.ingredient.all()])
+
+#Ingredients in Item model
+class IngredientsInItem(models.Model):
+    class Meta:
+        verbose_name = 'IngredientsInItem'
+        
+    item = models.ForeignKey(Item, on_delete=models.CASCADE)
+    ingredient = models.ForeignKey(Ingredient, on_delete=models.CASCADE)
+    quantity_of_ingredient = models.DecimalField(max_digits=10, decimal_places=4, null=True, blank=True)
+
+
+#Table and Seat Plan models
+class Table(models.Model):
+    table_no = models.IntegerField(primary_key=True)
+    party_size_capacity = models.IntegerField(null=True, blank=True)
+
+    def __str__(self):
+        return 'Table ' + str(self.table_no)
+
+class SeatPlan(models.Model):
+    party_size_capacity = models.IntegerField(null=True, blank=True)
+    table = models.ManyToManyField(Table)
+
+    def __str__(self):
+        return 'Seat Plan ' + str(self.id)
+
+    def tables(self):
+        if self.table:
+            return ', '.join([str(t.table_no) for t in self.table.all()])
+
+#Reservation model
+class Reservation(models.Model):
+    date_reserved = models.DateTimeField(auto_now_add=True)
+    check_in_date = models.DateTimeField()
+    party_size = models.IntegerField(null=True, blank=True)
+    seat_plan = models.ForeignKey(SeatPlan, on_delete=models.DO_NOTHING)
+
+    def __str__(self):
+        return str(self.id)
+
+
+#Order model
+class Order(models.Model):
+    date_ordered = models.DateTimeField(auto_now_add=True)
+    reservation_detail = models.ForeignKey(Reservation, on_delete=models.DO_NOTHING)
+    ordered_items = models.ManyToManyField(Item, through='OrderDetail')
+    offer = models.ForeignKey(Offer, on_delete=models.SET_NULL, null=True, blank=True)
+
+    def order_details(self):
+        if self.ordered_items:
+            orderdetails = OrderDetail.objects.filter(order=self.id)
+            return ', '.join([str(o.id) for o in orderdetails])
+
+    @property
+    def order_total_cost(self):
+        if self.ordered_items:
+            orderdetails = OrderDetail.objects.filter(order=self.id)
+            return sum([item.order_item_cost for item in orderdetails])        
+    @property
+    def order_total_cost_after_discount(self):
+        if self.ordered_items:
+            orderdetails = OrderDetail.objects.filter(order=self.id)
+            return sum([item.order_item_cost_after_discount for item in orderdetails])        
+
+    @property
+    def discount(self):
+        if self.offer:
+            return self.order_total_cost_after_discount * self.offer.discount_rate / 100
+        else:
+            return 0
+
+    @property
+    def total_cost_after_discount(self):
+        return self.order_total_cost_after_discount - self.discount
+
+    def __str__(self):
+        return str(self.id)
+
+#Order Detail model
+class OrderDetail(models.Model):
+    class Meta:
+        verbose_name = 'OrderDetail'
+
+    order = models.ForeignKey(Order, on_delete=models.DO_NOTHING)
+    item = models.ForeignKey(Item, on_delete=models.DO_NOTHING)
+    quantity = models.PositiveIntegerField(null=True, blank=True)
+    special_instruction = models.TextField(null=True, blank=True)
+
+    @property
+    def order_item_cost(self):
+        return self.quantity * self.item.cost
+
+    @property
+    def order_item_cost_after_discount(self):
+        return self.quantity * self.item.cost_after_discount
+
+    
