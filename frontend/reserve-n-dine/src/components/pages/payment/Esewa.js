5debe519bf3add3c5a24920b1bac291cfbe8f9eb
Ishaadhikary <isha.adhikary12354@gmail.com>
2021-08-10 21:10:05 +0545

    Esewa frontend

--- frontend/reserve-n-dine/src/components/pages/payment/Esewa.js
+++ frontend/reserve-n-dine/src/components/pages/payment/Esewa.js
@@ -1,27 +1,48 @@
+  
 import React from 'react';
+import axios from 'axios';
+import './Esewa.css';
 
 const Esewa = (props) => {
-    const { calculateOrderTotal, orderItems } = props;
+  const { calculateOrderTotal, orderItems } = props;
 
-    const currentOrderFromLocalStorage = JSON.parse(
-        localStorage.getItem('currentorder') || '[]'
-    );
-
-    return ( <>
-        <form action = "https://uat.esewa.com.np/epay/main" method = "POST" >
-        <input value = { calculateOrderTotal() } name = "tAmt"type = "text" />
-        <input value = { calculateOrderTotal() } name = "amt" type = "text" />
-        <input value = "0" name = "txAmt" type = "text" />
-        <input value = "0" name = "psc"type = "text" />
-        <input value = "0" name = "pdc" type = "text" />
-        <input value = "EPAYTEST" name = "scd" type = "text" />
-        <input value =  {currentOrderFromLocalStorage} name = "pid" type = "text" />
-        <input value = "https://reservendine.netlify.app/menu"
-        type = "text" name = "su" />
-        <input value = "https://reservendine.netlify.app/menu"  type = "text" name = "fu" >
-        </input> <input value = "Submit" type = "submit" >
-        </input> 
-        </form> </>
-    );
+  const currentOrderFromLocalStorage = JSON.parse(
+    localStorage.getItem('currentorder') || '[]'
+  );
+  const handlePayment = (e) => {
+    e.preventDefault();
+    axios
+      .post("api/paymentmethods/", {
+        payment_method_name: "eSewa",
+      })
+      .then((response) => {
+        console.log(response);
+      })
+      .catch((error) => console.log(error));
+    };
+  return (<>
+  
+      <div className="esewacontain"><h3>You will be redirected to esewa page</h3>
+      <button className="esewa "onClick={handlePayment}>Are you sure want to pay?</button> </div>
+      <form action="https://uat.esewa.com.np/epay/main" method="POST">
+       
+        <input value={calculateOrderTotal()} name="tAmt" type="hidden" />
+        <input value={calculateOrderTotal()} name="amt" type="hidden" />
+        <input value="0" name="txAmt" type="hidden" />
+        <input value="0" name="psc" type="hidden" />
+        <input value="0" name="pdc" type="hidden" />
+        <input value="EPAYTEST" name="scd" type="hidden" />
+        <input value={currentOrderFromLocalStorage} name="pid" type="hidden" />
+        <input
+          value="http://merchant.com.np/page/esewa_payment_success?q=su"
+          type="hidden"
+          name="su"
+        />
+        <input value="http://merchant.com.np/page/esewa_payment_failed?q=fu" type="hidden" name="fu"></input>
+        <div className="submit-alignment"><input value="Click here " type="submit" class="submit"></input></div>
+     
+      </form>
+    </>
+  );
 };
 export default Esewa;
--- frontend/reserve-n-dine/src/components/pages/payment/payment.js
+++ frontend/reserve-n-dine/src/components/pages/payment/payment.js
@@ -1,8 +1,24 @@
 import React from 'react'
+import axios from 'axios';
+import { Link } from 'react-router-dom';
 
 const Payment = () => {
-    return ( <div >
-        <h3 > Cash payment </h3>  
+        const handlePaymentCash = (e) => {
+            e.preventDefault();
+            axios
+                .post("api/paymentmethods/", {
+                    payment_method_name: "Cash on arrival",
+                })
+                .then((response) => {
+                    console.log(response);
+                })
+                .catch((error) => console.log(error));
+         };
+    return (
+        <div>
+            <h3>Your bill has been saved </h3> 
+             <button class="cash" onClick={handlePaymentCash}>
+  <Link to ="/"  style={{ textDecoration: "none", color:"inherit"}} >Exit</Link></button> 
         </div>
     )
 }
